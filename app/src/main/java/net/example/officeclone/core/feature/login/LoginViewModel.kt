523d8b03d1f33b5384da0747d3fceb3509270b17
JunghunKum <123260423+kumgold@users.noreply.github.com>
2024-12-13 21:41:43 +0900

    login ui

---
+++ app/src/main/java/net/example/officeclone/core/feature/login/LoginViewModel.kt
@@ -0,0 +1,15 @@
+package net.example.officeclone.core.feature.login
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor() : ViewModel() {
+    fun login(
+        userId: String,
+        password: String
+    ) {
+        
+    }
+}
