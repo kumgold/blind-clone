863240fa256a23348eb88ea0efc50581e10ca67b
JunghunKum <123260423+kumgold@users.noreply.github.com>
2025-06-06 16:03:04 +0900

    update post detail

--- app/src/main/java/com/example/blindclone/ui/component/TopAppBar.kt
+++ app/src/main/java/com/example/blindclone/ui/component/TopAppBar.kt
@@ -1,9 +1,15 @@
 package com.example.blindclone.ui.component
 
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ChatBubbleOutline
+import androidx.compose.material.icons.filled.IosShare
 import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.NotificationsOff
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Bookmark
+import androidx.compose.material.icons.outlined.BookmarkBorder
+import androidx.compose.material.icons.outlined.MoreHoriz
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -11,7 +17,9 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.blindclone.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -56,6 +64,62 @@ fun MainTopAppBar(
                 Icon(
                     imageVector = Icons.Outlined.Person,
                     contentDescription = "My Page"
+                )
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PostAppBar(
+    popBackStack: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(text = stringResource(id = R.string.blind))
+        },
+        navigationIcon = {
+            IconButton(
+                onClick = { popBackStack() }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.ArrowBackIosNew,
+                    contentDescription = "Back"
+                )
+            }
+        },
+        actions = {
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.IosShare,
+                    contentDescription = "Share"
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Default.NotificationsOff,
+                    contentDescription = "Notification"
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.BookmarkBorder,
+                    contentDescription = "Bookmark"
+                )
+            }
+            IconButton(
+                onClick = { /*TODO*/ }
+            ) {
+                Icon(
+                    imageVector = Icons.Outlined.MoreHoriz,
+                    contentDescription = "Settings"
                 )
             }
         }
